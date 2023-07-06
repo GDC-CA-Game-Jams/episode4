938e757504cfabf8464016482304ffb4485b2219
JulieBodette <jr3python@gmail.com>
2023-07-06 00:34:05 -0400

    Added button to go from main menu to game scene

--- Assets/Code/LoadSceneOnClick.cs
+++ Assets/Code/LoadSceneOnClick.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadSceneOnClick : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnClick(string name)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(name);
     }
 }
