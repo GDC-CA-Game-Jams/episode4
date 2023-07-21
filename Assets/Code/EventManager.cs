550a265221e82e8c179413f71e588f69be2feb79
Doug Guzman <swiftshadow34@gmail.com>
2023-07-20 22:16:33 -0500

    Adds base functionality of the glamour bar - adding when hitting a note, removing when missing, and death. Bar smoothly slides to its next value

--- Assets/Code/EventManager.cs
+++ Assets/Code/EventManager.cs
@@ -10,20 +10,32 @@ public class EventManager : IService
     public UnityEvent OnHit = new();
     public UnityEvent OnMiss = new();
 
+    public UnityEvent OnDeath = new();
+
+    private float discoMeterHitIncreaseAmount = 10;
+    private float discoMeterMissDecreaseAmount = -10;
 
     public void HitListener()
     {
         Debug.Log("Hit!");
+        ServiceLocator.Instance.Get<DiscoMeterService>().ChangeValue(discoMeterHitIncreaseAmount);
     }
 
     public void MissListener()
     {
         Debug.Log("Miss!");
+        ServiceLocator.Instance.Get<DiscoMeterService>().ChangeValue(discoMeterMissDecreaseAmount);
     }
 
+    private void DeathListener()
+    {
+        Debug.Log("Player died!");
+    }
+    
     public void Init()
     {
         OnHit.AddListener(HitListener);
         OnMiss.AddListener(MissListener);
+        OnDeath.AddListener(DeathListener);
     }
 }
