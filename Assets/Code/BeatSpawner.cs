539e20b6c9b3723debd80285fe689147180c1784
Bretterbear <brettney40hands@gmail.com>
2023-07-25 16:05:44 -0700

    Random Beat Spawning
    Created a random spawn beat system that'll just spawn random beats on up/down/left/right locales. NOTE - YOU NOW HAVE TO USE THE BEATSPAWNER PREFAB AS IT HAS THE CORRECT REFERENCES LOADED.

--- Assets/Code/BeatSpawner.cs
+++ Assets/Code/BeatSpawner.cs
@@ -6,16 +6,17 @@ using UnityEngine.UIElements;
 public class BeatSpawner : MonoBehaviour
 {
     [SerializeField] private GameSettingsSO m_Settings = null;
-    
+
     public Orientation orientation = Orientation.Horizontal;
+    public ReadMode readmode = ReadMode.Random;
 
-    public float beatTempo;
+    public float beatTempo; //gets auto overridden by settings beatTempo
 
-    public float beatFrequency;
+    public float beatFrequency; //gets auto overridden by settings beatFrequency
 
     public GameObject notePrefab;
 
-    public Transform spawnPoint;
+    public Transform SpawnPointUp, SpawnPointDown, SpawnPointLeft, SpawnPointRight;
 
     private float clock = 0f;
 
@@ -29,25 +30,52 @@ public class BeatSpawner : MonoBehaviour
     private void Update()
     {
         clock += Time.deltaTime;
-        if(clock > beatFrequency)
+        if (clock > beatFrequency)
         {
             clock = 0f;
-            var noteObject = Instantiate(notePrefab);
-            var noteObjectScript = noteObject.GetComponent<NoteObject>();
 
-            noteObject.transform.SetParent(gameObject.transform, false);
-            noteObject.transform.position = spawnPoint.position;
+            if (readmode == ReadMode.Random)
+            {
+                RandomSpawn();
+            }
+        }
+    }
+    private void RandomSpawn()
+    {
+        var noteObject = Instantiate(notePrefab);
+        var noteObjectScript = noteObject.GetComponent<NoteObject>();
 
-            if(orientation == Orientation.Horizontal)
-            {
-                noteObjectScript.rb = noteObject.GetComponent<Rigidbody2D>();
-                noteObjectScript.rb.velocity = new Vector2(beatTempo * -1, 0f);
-            }
-            else if(orientation == Orientation.Vertical)
-            {
-                noteObjectScript.rb = noteObject.GetComponent<Rigidbody2D>();
-                noteObjectScript.rb.velocity = new Vector2(0f, beatTempo * -1);
-            }
+        noteObject.transform.SetParent(gameObject.transform, false);
+
+        int direction = UnityEngine.Random.Range(0, 4);
+        switch(direction)
+        {
+            case 0: //down
+                noteObject.transform.position = SpawnPointDown.position;
+                break;
+            case 1: //right
+                noteObject.transform.position = SpawnPointRight.position;
+                noteObject.transform.Rotate(new Vector3(0, 0, 90));
+                break;
+            case 2: //up
+                noteObject.transform.position = SpawnPointUp.position;
+                noteObject.transform.Rotate(new Vector3(0, 0, 180));
+                break;
+            case 3: //left
+                noteObject.transform.position = SpawnPointLeft.position;
+                noteObject.transform.Rotate(new Vector3(0, 0, 270));
+                break;
+        }
+
+        if (orientation == Orientation.Horizontal)
+        {
+            noteObjectScript.rb = noteObject.GetComponent<Rigidbody2D>();
+            noteObjectScript.rb.velocity = new Vector2(beatTempo * -1, 0f);
+        }
+        else if (orientation == Orientation.Vertical)
+        {
+            noteObjectScript.rb = noteObject.GetComponent<Rigidbody2D>();
+            noteObjectScript.rb.velocity = new Vector2(0f, beatTempo * -1);
         }
     }
 }
@@ -56,4 +84,10 @@ public enum Orientation
 {
     Horizontal,
     Vertical,
+}
+
+public enum ReadMode
+{
+    Random,
+    Read,
 }
