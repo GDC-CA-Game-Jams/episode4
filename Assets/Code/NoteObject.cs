6789da497105c4944a0b6b40a3020cb1fc9230c5
Bretterbear <brettney40hands@gmail.com>
2023-08-04 11:30:48 -0700

    Long Handling
    Handling for long notes in input buttons (need to set up handling in spawning)

--- Assets/Code/NoteObject.cs
+++ Assets/Code/NoteObject.cs
@@ -8,29 +8,32 @@ using UnityEngine;
 
 public class NoteObject : MonoBehaviour
 {
+    //public (serialized) variables
     [Header("Difficulty Variables")]
     [Tooltip("Point worth of note on a hit (modified by grade on hit)")]
     public float pointValue = 10f;
     [Tooltip("specifies how much a miss costs the player in terms of glamour")]
     public float missValue = -10f;
 
-    [Header("Reference Variables")]
-    public Rigidbody2D rb;
+    //public (non-serialized) variables
+    [NonSerialized] public Rigidbody2D rb;
 
     //private variables
-    private bool canBeActivated; //indicates whether the note is in the "zone" of a button
+    private bool bLongNote = false;
+    private bool bObstacleNote = false;
+
 
     private void OnTriggerEnter2D(Collider2D other)
     {
 
-        if(other.CompareTag("Destroyer"))
+        if (other.CompareTag("Destroyer"))
         {
-            ServiceLocator.Instance.Get<DiscoMeterService>().ChangeValue(missValue);
             ServiceLocator.Instance.Get<EventManager>().OnMiss?.Invoke(); //might pull this if we don't add things into it
             Destroy(gameObject);
         }
     }
 
+    //getters & setters
     public float getRawScoreWorth()
     {
         return pointValue;
@@ -39,5 +42,25 @@ public class NoteObject : MonoBehaviour
     public float getRawMissWorth()
     {
         return missValue;
+    }
+
+    public bool GetFlagIsLongNote()
+    {
+        return bLongNote;
+    }
+
+    public void SetFlagIsLongNote(bool flag)
+    {
+        bLongNote = flag;
+    }
+
+    public bool GetFlagIsObstacleNote()
+    {
+        return bObstacleNote;
+    }
+
+    public void SetFlagIsObstacleNote(bool flag)
+    {
+        bObstacleNote = flag;
     }
 }
